3251deb0e0cfab6278dfd114576ba63fa632ab39
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-07-16 21:56:09 +0530

    Code changes made for the submission.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function fibonacci(num) {
-    let fib = [0, 1];
-    for(let i = 2; i <= num; i++){
-        fib[i] = fib[i - 1] + fib[i - 2];
+    if (num <= 0) {
+        throw new Error("Input number must be a positive integer");
     }
-    return fib[num];
+    else if (num === 1) {
+        return 0;
+    }
+    else if (num === 2) {
+        return 1;
+    }
+
+    let a = 0; b = 1;
+    for (let i = 0;  i <= num - 2; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+
+    return b;
 }
 module.exports = fibonacci;
